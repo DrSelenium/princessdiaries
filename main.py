93babc0765227dfa0666d7bb18f86290ae90ae17
DrSelenium <scisofun@gmail.com>
2025-09-06 10:30:46 +0800

    Answers filled

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ app = fastapi.FastAPI()
 
 @app.get("/trivia")
 def read_root():
-    return {"answers": []}
+    return {"answers": [
+        2,
+        1,
+        2,
+        2,
+        3,
+        4,
+        2,
+        5,
+        4
+
+    ]}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
